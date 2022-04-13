b49271aaa40ce33262572693bd6351b9257c2d4c
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-13 18:17:11 -0300

    Layout and load todos

--- src/views/OptionsView.vue
+++ src/views/OptionsView.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>You have XXX Todos!</h3>
+    <div>
+      <input type="text" placeholder="Add a Todo">
+    </div>
+  </div>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <span>{{ todo.name }}</span>
+        <button>X</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'OptionsView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [
+        {id: 1, name: 'One'},
+        {id: 2, name: 'Two'},
+        {id: 3, name: 'Three'},
+      ]
+    }
   }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  width: 200px;
+  margin: 20px auto 0;
+}
+
+li {
+  border: 1px solid;
+  display: flex;
+  margin-bottom: 10px;
+}
+
+li span {
+  flex-grow: 1;
+}
+</style>
